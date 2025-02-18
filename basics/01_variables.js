82ba2a8332706a240b0f0c8653f4d937583b9aa3
Priyanshu Soni <136926598+Priyanshusoni3@users.noreply.github.com>
2025-02-18 08:29:05 +0000

    Container config done

---
+++ basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1235356;//we cannot change const value
+let name="Priyasnhu";
+var accountEmail="priyanshu@gmail.com";
+
+newAccount=65894454;
+let age;
+console.table([accountId,name,accountEmail,newAccount,age]);
+
+/*Prefer not to use var beacause of functional and block scope
+*/
+
